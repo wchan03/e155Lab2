d07492749271ae3bb6c03a93e79f760d304081c1
wchan03 <123441599+wchan03@users.noreply.github.com>
2025-09-05 13:28:19 -0700

    Initializing Lab 2 Repo

---
+++ fpga/src/lab2_wc.sv
@@ -0,0 +1,16 @@
+// Wava Chan
+// wchan@g.hmc.edu
+// Sept. 4, 2025
+// Top Level Module for Lab 2: Mutiplexed 7-Seg Display
+// This module TODO: FINISH This
+
+module lab2_wc(input logic clk, reset
+                input logic [3:0] s1, s2, // Two DIP switches
+                output logic [6:0] seg_disp1, seg_disp2);
+
+                // Segment Display Module
+
+
+                //TO DO 
+                // Module (?) to drive the switching between two pins
+                
